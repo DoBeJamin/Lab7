534f7b2c4aa0f619889de264d89693bf064a5464
DoBeJamin <benjaminick11@gmail.com>
2023-06-30 11:32:39 -0500

    final project

--- src/Lab7.ino
+++ src/Lab7.ino
@@ -7,73 +7,76 @@
 
 SYSTEM_THREAD(ENABLED);
 
-
 OledWingAdafruit display;
 
 #define TEMPERATURE A5
 
+int timer = 0;
+
 void setup()
 {
-  display.setup();
+
 
   pinMode(TEMPERATURE, INPUT);
 
-  Serial.begin(9600);
-
-  delay(5000); 
+  //setup
   Blynk.begin(BLYNK_AUTH_TOKEN);
-
+  display.setup();
+  Serial.begin(9600);
 
 }
 
 void loop()
 {
+
+  // update display and blynk
   display.loop();
   Blynk.run();
 
 
 
   if (display.pressedA()) {
-    Blynk.logEvent("buttona", "panic button A has been pressed");
+    //notify blynk
+    Blynk.logEvent("buttona", "panic button A has been pressed"); 
+    Serial.print("alert"); //temporary alery because I ran out of events 
+
+    //displays "push sent" message and sets timer
     format_display();
     display.println("Push Sent");
     display.display();
-    delay(5000);
+    timer = 20000;
 
+  } else if (!timer) {
+
+    //calculate temperatures
+    uint64_t reading = analogRead(A5);
+    double voltage = (reading * 3.3) / 4095.0;
+    double temperature_celcius = (voltage - 0.5) * 100;
+    double temperature_fahrenheit = (temperature_celcius * 9.0 / 5.0) + 32.0;
+
+    //format screen and text
+    format_display();
+    display.println("TEMPERATURE");
+    display.print(temperature_celcius);
+    display.print("C : ");
+    display.print(temperature_fahrenheit);
+    display.print("F");
+
+    //display screen
+    display.display();
+
+    //Blynk.run();
+    Blynk.virtualWrite(V0, temperature_fahrenheit);
+    Blynk.virtualWrite(V1, temperature_celcius);
   }
-
-
-
-
-
-
-
- 
-  //calculate temperatures
-  uint64_t reading = analogRead(A5);
-  double voltage = (reading * 3.3) / 4095.0;
-  double temperature_celcius = (voltage - 0.5) * 100;
-  double temperature_fahrenheit = (temperature_celcius * 9.0 / 5.0) + 32.0;
-
-  //format screen and text
-  format_display();
-  display.println("TEMPERATURE");
-  display.print(temperature_celcius);
-  display.print("C : ");
-  display.print(temperature_fahrenheit);
-  display.print("F");
-
-  display.display();
-
-
-
-  //Blynk.run();
-  Blynk.virtualWrite(V0, temperature_fahrenheit);
-  Blynk.virtualWrite(V1, temperature_celcius);
-
+  //countdown before switching back to temperature displaying
+  if (timer > 0) {
+    timer--;
+  }
 }
 
 void format_display () {
+  //sets up basic formatting for when using the display
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE); 
